4c6bcd175a5c5e803810ab3aed0fe9829ff9ffac
Nirbhay2002 <110183517+Nirbhay2002@users.noreply.github.com>
2023-06-21 23:22:16 +0530

    Fetching error fixed

--- src/App.js
+++ src/App.js
@@ -4,31 +4,33 @@ import React from 'react';
 function App() {
 
   const [currentWeather, setCurrentWeather] = React.useState({});
+  // const [isPending, setIsPending] = React.useState(true);
   const url="http://api.weatherstack.com/current?access_key=a9e46baa27e226e744e600d52ef3cc2f&query=Chandigarh";
 
   React.useEffect(() =>{
     fetch(url)
     .then(res => res.json())
-    .then(data => setCurrentWeather(data.current))
-  }, []);  
+    .then(data => {
+      // setIsPending(false);
+      setCurrentWeather(data.current);
+    })
+  }, [url]);  
 
-  const weatherData = {
-    "city": "Chandigarh",
-    "temp": currentWeather.temperature,
-    "time": currentWeather.observation_time,
-    "tod": currentWeather.is_day === "yes" ? "Day": "Night",
-    "weather": currentWeather.weather_descriptions,
-    "icon": currentWeather.weather_icons,
-    "humidity": currentWeather.humidity, //in %
-    "wind_speed": currentWeather.wind_speed, //in kmph
-    "precipitation": currentWeather.precip
-  };
+  // const weatherData = {
+  //   "city": "Chandigarh",
+  //   "temp": currentWeather.temperature,
+  //   "time": currentWeather.observation_time,
+  //   "tod": currentWeather.is_day === "yes" ? "Day": "Night",
+  //   "weather": currentWeather.weather_descriptions,
+  //   "icon": currentWeather.weather_icons,
+  //   "humidity": currentWeather.humidity, //in %
+  //   "wind_speed": currentWeather.wind_speed, //in kmph
+  //   "precipitation": currentWeather.precip
+  // };
 
   return (
     <div className="App">
-      <div className="card-container">
-        <Card cityWeather = {weatherData}/>
-      </div>
+        {currentWeather && <Card cityWeather = {currentWeather} city="Chandigarh"/>}
     </div>
   );
 }
--- src/Card.js
+++ src/Card.js
@@ -1,19 +1,18 @@
-const Card = ({cityWeather}) => {
+const Card = ({cityWeather, city}) => {
 
     const styleBackground ={
         backgroundColor: cityWeather.tod === "Day" ? "linear-gradient(to bottom right, rgb(6, 123, 201), rgb(255, 255, 255))": "black",
-    }
+    };
     const styleContent = {
         color: cityWeather.tod === "Day" ? "black" : "white"
-    }
+    };
     return ( 
-        
                 <div className="card-info">
                     <div className="card-left" style={styleBackground}>
-                        <h1 style={styleContent}>{cityWeather && cityWeather.city}</h1>
-                        <p style={styleContent}>{cityWeather && cityWeather.temp}<sup>o</sup>C</p>
-                        <p style={styleContent}>{cityWeather && cityWeather.tod}</p>
-                        <p style={styleContent}>{cityWeather && cityWeather.weather}</p>
+                        <h1 style={styleContent}>{city}</h1>
+                        <p style={styleContent}>{cityWeather.is_Day? "Day": "Night"}</p>
+                        <h1 style={styleContent}>{cityWeather.temperature}<sup>o</sup>C</h1>
+                        <p style={styleContent}>{cityWeather.weather_descriptions}</p>
                     </div>
                     <div className="card-right">
                         <div className="card-right-labels">
@@ -22,9 +21,9 @@ const Card = ({cityWeather}) => {
                             <p>WIND </p>
                         </div>
                         <div className="card-right-details">
-                            <p>{cityWeather && cityWeather.precipitation} mm</p>
-                            <p>{cityWeather && cityWeather.humidity}%</p>
-                            <p>{cityWeather && cityWeather.wind_speed}%</p>
+                            <p>{cityWeather.precip} mm</p>
+                            <p>{cityWeather.humidity}%</p>
+                            <p>{cityWeather.wind_speed} km/h</p>
                         </div> 
                     </div>
                 </div>
