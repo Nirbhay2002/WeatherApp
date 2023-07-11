0e01b0288fda85d027d3e6eec66dda2592ca2dbe
Nirbhay2002 <110183517+Nirbhay2002@users.noreply.github.com>
2023-07-11 18:26:46 +0530

    github deployment removed

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import Navbar from './Navbar';
 import React from 'react';
 
 function App() {
-  
-  const apiKey = "CTciAfIaBaELQ3MK6xkCcMgavG5xIGdj";
- 
+
   const [country, setCountry] = React.useState("India");
   const [weatherData, setWeatherData] = React.useState([]);
-
+  // const [locationKey, setLocationKey] = React.useState("");
   
+  const apiKey = "CTciAfIaBaELQ3MK6xkCcMgavG5xIGdj";
+  // const locationApi = `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${apiKey}&q=${country}`
+  // const currentConditionsApi = `http://dataservice.accuweather.com/currentconditions/v1/${locationKey}?apikey=${apiKey}`
 
   
   const newUrl = `http://dataservice.accuweather.com/currentconditions/v1/topcities/50?apikey=${apiKey}`;
@@ -31,9 +32,22 @@ function App() {
   const handleChange = (event) =>{
     setCountry(event.target.value);
 };
+
+  // const handleSubmit = (e) =>{
+  //   e.preventDefault();
+  //   fetch(locationApi)
+  //   .then(res => res.json())
+  //   .then(data => setLocationKey(data.Key))
+  //   .then(
+  //     fetch(currentConditionsApi)
+  //     .then(res=>res.json())
+  //     .then(data => setWeatherData(data))
+  //   )
+
+  // }
   return (
     <div className="App">
-      <Navbar weather= {weatherData} handleChange={handleChange}/>
+      <Navbar weather= {weatherData} handleChange={handleChange} />
 
       <h1 className="content-heading">Showing Weather for {country}</h1>
       {locationData}
